bff29f6afc3a7c69f4630368a46e1374678f7628
geraldAp <117451278+geraldAp@users.noreply.github.com>
2023-04-06 23:31:26 +0100

    2

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
       <div>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
-        <Link href="/ninjas">Ninja Listing</Link>
+        <Link href="/ninjas">Listing</Link>
       </div>
     </nav>
   );
